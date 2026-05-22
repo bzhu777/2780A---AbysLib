e6d139993f855db837005ac7fa485e02b139c151
bzhu777 <bzhu201208@gmail.com>
2026-05-22 16:16:28 -0400

    variables setup for mpc

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -23,7 +23,36 @@ using namespace vex;
 // Forward velocity
 // Angular velocity
 
+const static float trackWidth = 13.5; // in inches
+const static int horizon = 10; // how many steps to predict into the future
+
+float linearVelocity = 0; // forward velocity
+float angularVelocity = 0; // how fast the robot is turning
+
+const float maxLeftVelocity = wheelToMotorRatio*600/60*wheelDiam*M_PI; // max left motor velocity, inches per second
+const float maxRightVelocity = wheelToMotorRatio*600/60*wheelDiam*M_PI; // max right motor velocity, inches per second
+
+const float maxLinearVelocity = (maxLeftVelocity+maxRightVelocity)/2; // max forward velocity, inches per second
+const float maxAngularVelocity = (maxLeftVelocity+maxRightVelocity)/trackWidth; // max turning speed in radians per second
+
+
+float bestCost = 1e9;
+float bestControl[2] = {0, 0}; // {linear velocity, angular velocity}
+
+float simulatedX=0;
+float simulatedY=0;
+float simulatedHeading=0;
+float totalCost=0;
+
+
 void predictFutureState(double goalX, double goalY)
 {
+    simulatedX = Xpos;
+    simulatedY = Ypos;
+    simulatedHeading = ABSorientation;
 
+    for (int i = 0; i < horizon; i++) {
+        // Simple kinematic model
+        
+    }
 }
